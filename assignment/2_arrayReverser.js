0f3fe51c75ac789568676030c0f926d33b630774
Kris Szafranski <kdszafranski@gmail.com>
2017-06-02 12:30:19 -0500

    added instructions back into solution files

--- assignment/2_arrayReverser.js
+++ assignment/2_arrayReverser.js
@@ -1,11 +1,14 @@
 function arrayReverser(arr) {
-  // copy items from arr to revArray in reverse order.
-  // write the code here
-  var revArray = [];
+  // Copy items from the array called arr to a new array called reversedArray, reversing the order of elements
+  // The last element on arr should become the first in reversedArray
+  // Do not use Array.reverse()!
+
+  // Write the code here
+  var reversedArray = [];
   for (var i = arr.length - 1; i >= 0; i--) {
-    revArray.push(arr[i]);
+    reversedArray.push(arr[i]);
   }
 
   // return our reversed array
-  return revArray;
+  return reversedArray;
 }
