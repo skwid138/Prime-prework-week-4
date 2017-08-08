a3d8fd83949516a32553ce1daa6ae325aed80699
skwid138 <skwid138@gmail.com>
2017-08-08 14:42:29 -0500

    completed assignment 4

--- assignment/4_objectLiteralFix.js
+++ assignment/4_objectLiteralFix.js
@@ -1,16 +1,17 @@
 function fixLiteralSyntax() {
   // Fix the problems with our object literal
-  /**
+
   hiddenFiguresMovie = {
-      name : Hidden Figures;
-      runtime : 127 mins;
-      releaseYear = 2016;
-      "director" : "Theodore Melfi".
-  }
-  **/
+      name : 'Hidden Figures', // put the string in '', removed semicolon, and added comma
+      runtime : '127 mins', // put the string in '', removed semicolon, and added comma
+      releaseYear : 2016, // removed =, added colon, removed semicolon and added comma
+      director : "Theodore Melfi"  // removed '.' after the string and " from key
+  }; // added semicolon
+
 
   // return corrected object
   return hiddenFiguresMovie;
 }
 
 // call fixLiteralSyntax and console log the return
+console.log(fixLiteralSyntax());
