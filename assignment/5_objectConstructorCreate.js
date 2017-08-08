ea57d66909eb3009a4083bb1b866834dd25f6d65
skwid138 <skwid138@gmail.com>
2017-08-08 15:28:50 -0500

    finished assignment 5... I think

--- assignment/5_objectConstructorCreate.js
+++ assignment/5_objectConstructorCreate.js
@@ -3,18 +3,33 @@
 // Write the constructor function here:
 function Book(title, author, pageCount, haveRead) {
   // Write code here
-
+  this.title = title;
+  this.author = author;
+  this.pageCoiunt = pageCount;
+  this.haveRead = haveRead;
 }
 
 function createBooksWithConstructor() {
   // In this function, create 3 new 'Book' objects by using the constructor function you wrote above.
   // Write code here:
-
+  // for (let i = 0; i < 3; i ++) {
+  //   var book = 'book';
+  //   book += i.toString();
+  //   book = new Book('Unstoppable','Bill Nye', 352, true)
+  //   }
+ var bookOne = new Book('Unstoppable','Bill Nye', 352, true);
+ var bookTwo = new Book('Origins: Fourteen Billion Years of Cosmic Evolution', 'Neil DeGrasse Tyson', 352, true);
+ var bookThree = new Book('The Innovators: How a Group of Hackers, Geniuses, and Geeks Created the Digital Revolution', 'Walter Isaacson', 560, true);
   // Store these new books in an array called books.
   // Write code here
-
+  var books = [
+    bookOne,
+    bookTwo,
+    bookThree
+  ];
   // return our array of books
   return books;
 }
 
 // call createBooksWithConstructor and console log the return
+console.log(createBooksWithConstructor());
